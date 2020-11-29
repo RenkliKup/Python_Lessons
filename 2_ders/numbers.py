3937953f4b0ea688da39a4983b34499e88f12346
Baris Karakulak <baris.1907.krklk@gmail.com>
2020-11-29 22:51:33 +0300

    2_ders versiyon 3

--- 2_ders/numbers.py
+++ 2_ders/numbers.py
@@ -1,3 +1,6 @@
 print(2+2) #ekrana 4 yazar
 print(type(2.0))#float veri türü gösterir
 print(type(2))#int veri türü gosterir
+print(2**3)#** operatoru üssünü alır
+print(10//3)#"//" operatoru sayıyı boler ama float kısmını yazmaz sayıyı aşağıya yuvarlar
+print(-10//3)#aşağıya yuvarlar ve -4 olur çıktı
