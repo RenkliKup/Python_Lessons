95873ff3a4e00a2b6c36a9110e1b10fe9a1c2a8a
Baris Karakulak <baris.1907.krklk@gmail.com>
2020-12-31 19:59:33 +0300

    modul eklendi 2

---
+++ Modul_Kullanimi/my_new_module/lecture.py
@@ -0,0 +1,9 @@
+import main
+'''
+result=main.number
+result=main.func(10)
+result=main.person()
+'''
+result=dir(main)
+result=help(main)
+print(result)
---
+++ Modul_Kullanimi/my_new_module/main.py
@@ -0,0 +1,21 @@
+'''
+Wride by b.k
+'''
+print("modül eklendi")
+
+
+number=10
+
+
+def func(x):
+    '''
+    my func
+    '''
+    return f"X:{x}"
+
+class person:
+    '''
+    my class
+    '''
+    def __init__(self):
+        print("oluştu")
