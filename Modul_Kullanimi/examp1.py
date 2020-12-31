acd508174d2367da8c02f529da3c58617f055ad2
Baris Karakulak <baris.1907.krklk@gmail.com>
2020-12-31 19:32:33 +0300

    modul eklendi

---
+++ Modul_Kullanimi/examp1.py
@@ -0,0 +1,19 @@
+'''
+yöntem 1
+import math
+
+value =dir(math)
+value=help(math)
+value=help(math.ceil)
+yontem 2
+import math as islem
+value=islem.ceil(7.5)
+yontem 3
+from math import *
+value=factorial(5)
+yontem 4
+from math import factorial,ceil
+value=ceil(4.6)
+
+print(value)
+'''
---
+++ Modul_Kullanimi/random_modul_kullanımı.py
@@ -0,0 +1,22 @@
+import random as rnd
+
+'''
+result=rnd.random()#0.0 1.0 arası
+result=rnd.random()*100
+result=rnd.uniform(1,100)#1 100 ARASI
+result=int(rnd.uniform(0,10)
+names=["baris","karakulak","beyza","babam","annem"]
+result=rnd.randint(0,len(names)-1)
+names=["baris","karakulak","beyza","babam","annem"]
+result=rnd.choice(names)
+greeting="Hello There"
+result=rnd.randint(0,len(greeting)-1)
+greeting="Hello There"
+result=rnd.choice(greeting)
+name=range(100)
+
+print(rnd.sample(name,3))
+name=range(100)
+
+print(rnd.sample(name,3))
+'''
